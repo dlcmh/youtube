8b4743a0e16a4c9fa823b09953a282a5e5a0bddf
David Chin <dlcmhd@me.com>
2016-10-11 21:49:41 +0800

    Auto Layout
    Horizontal axis:
    addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
    
    Vertical axis:
    addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
    
    Also turn off:
    imageView.translatesAutoresizingMaskIntoConstraints = false

--- YouTube/HomeController.swift
+++ YouTube/HomeController.swift
@@ -43,12 +43,15 @@ class VideoCell: UICollectionViewCell {
     let thumbnailImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = UIColor.blue
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     func setupViews() {
         addSubview(thumbnailImageView)
-        thumbnailImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": thumbnailImageView]))
     }
     
     required init?(coder aDecoder: NSCoder) {
