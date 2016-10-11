0a960839a380fec2555deb258725cd0d44744005
David Chin <dlcmhd@me.com>
2016-10-11 20:11:32 +0800

    cellForItemAt

--- YouTube/HomeController.swift
+++ YouTube/HomeController.swift
@@ -14,11 +14,18 @@ class HomeController: UICollectionViewController {
         super.viewDidLoad()
         
         navigationItem.title = "Home"
+
         collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        return cell
     }
 }
 
