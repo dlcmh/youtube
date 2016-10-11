688ee43493b99455c60ee2aad85800e9d6b9c527
David Chin <dlcmhd@me.com>
2016-10-11 21:27:39 +0800

    UICollectionViewDelegateFlowLayout, sizeForItemAt
    Cell size with CGSize(width: view.frame.width, height: 200)
    
    Grid layout

--- YouTube/HomeController.swift
+++ YouTube/HomeController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController {
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,10 @@ class HomeController: UICollectionViewController {
         cell.backgroundColor = UIColor.red
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
 }
 
