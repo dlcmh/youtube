8a77b9e7cb194ed05fdce7e05917871f68323e89
David Chin <dlcmhd@me.com>
2016-10-13 10:04:13 +0800

    Follow video's navigationItem.titleView approach

--- YouTube/HomeController.swift
+++ YouTube/HomeController.swift
@@ -13,12 +13,20 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.title = "Home"
+//        navigationItem.title = "Home"
         navigationController?.navigationBar.isTranslucent = false
         
-        // Used http://timdietrich.me/blog/swift-ios-customizing-navigation-and-tab-bar-appearance/ instead of video's use of a custom UILabel that additionally hardcodes a left position for the label - if required, re-visit the video at https://www.youtube.com/watch?v=APQVltARKF8 17:00
-        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        // http://timdietrich.me/blog/swift-ios-customizing-navigation-and-tab-bar-appearance/ instead of video's use of a custom UILabel that additionally hardcodes a left position for the label - if required, re-visit the video at https://www.youtube.com/watch?v=APQVltARKF8 17:00
+//        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
+        // Video's approach for navigationItem "title"
+        let navigationItemTitleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width - 32, height: view.frame.height))
+        navigationItemTitleLabel.text = "Home"
+        navigationItemTitleLabel.textColor = UIColor.white
+        navigationItemTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        navigationItem.titleView = navigationItemTitleLabel
 
+        // Set up collection view
         collectionView?.backgroundColor = UIColor.white
         collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellId")
     }
